f246a6cd8c71daf165ab5db26541d5e6f45d6099
Zafreen Tabassum Eshika <74498380+zafreeneshika@users.noreply.github.com>
2023-03-09 23:34:37 +0600

    DashBoard

---
+++ Lab-08-Task/3/DashBoard.java
@@ -0,0 +1,13 @@
+package code3;
+
+public class DashBoard {
+    public double size;
+    public DashBoard(double size){
+        this.size = size;
+
+    }
+
+    public double getSize() {
+        return size;
+    }
+}
