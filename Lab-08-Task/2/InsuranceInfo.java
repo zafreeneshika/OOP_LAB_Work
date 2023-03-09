8f805c061d0eda95fa20aad520773110b46fd10e
Zafreen Tabassum Eshika <74498380+zafreeneshika@users.noreply.github.com>
2023-03-09 23:30:45 +0600

    InsuranceInfo

---
+++ Lab-08-Task/2/InsuranceInfo.java
@@ -0,0 +1,10 @@
+package code2;
+
+public class InsuranceInfo {
+    String policyName;
+    int policyId;
+    public InsuranceInfo(String policyName,int policyId){
+        this.policyName = policyName;
+        this.policyId = policyId;
+    }
+}
