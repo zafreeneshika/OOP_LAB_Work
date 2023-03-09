a7b9c609488fe5c0786d118685bb9a0212ef6935
Zafreen Tabassum Eshika <74498380+zafreeneshika@users.noreply.github.com>
2023-03-09 23:35:37 +0600

    Engine

---
+++ Lab-08-Task/3/Engine.java
@@ -0,0 +1,12 @@
+package code3;
+
+public class Engine {
+    public double capacity;
+    public Engine(double capacity){
+        this.capacity = capacity;
+    }
+
+    public double getCapacity() {
+        return capacity;
+    }
+}
